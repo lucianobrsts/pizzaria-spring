726d9f5a7df044623585bcee54bab1017027b96c
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-03-10 21:58:08 -0300

    Implementado busca no banco e modificado configuração para update

--- pizzaria-spring/src/main/java/br/com/pizzaria/controladores/IngredienteController.java
+++ pizzaria-spring/src/main/java/br/com/pizzaria/controladores/IngredienteController.java
@@ -1,15 +1,27 @@
 package br.com.pizzaria.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.pizzaria.modelo.entidades.Ingrediente;
+import br.com.pizzaria.modelo.repositorios.IngredienteRepositorio;
 
 @Controller
 @RequestMapping("/ingredientes")
 public class IngredienteController {
 
+	@Autowired
+	private IngredienteRepositorio ingredienteRepositorio;
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listarIngredientes() {
+	public String listarIngredientes(Model model) {
+		Iterable<Ingrediente> ingredientes = ingredienteRepositorio.findAll();
+
+		model.addAttribute("titulo", "Listagem de Ingredientes");
+		model.addAttribute("ingredientes", ingredientes);
 
 		return "ingrediente/listagem";
 	}
