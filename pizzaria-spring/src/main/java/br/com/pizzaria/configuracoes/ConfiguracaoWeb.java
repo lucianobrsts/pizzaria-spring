e1fb0981e83d855c0b3c9294495865b50199bf32
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-03-28 14:14:33 -0300

    Configurado e criado arquivo de internacionalização

--- pizzaria-spring/src/main/java/br/com/pizzaria/configuracoes/ConfiguracaoWeb.java
+++ pizzaria-spring/src/main/java/br/com/pizzaria/configuracoes/ConfiguracaoWeb.java
@@ -1,10 +1,19 @@
 package br.com.pizzaria.configuracoes;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -20,6 +29,30 @@ public class ConfiguracaoWeb extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		registry.viewResolver(viewResolver);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor localeIntercptor = new LocaleChangeInterceptor();
+		localeIntercptor.setParamName("lang");
+
+		registry.addInterceptor(localeIntercptor);
+	}
+
+	@Bean
+	public LocaleResolver sessionLocaleResolver() {
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(new Locale("pt_BR"));
+		return resolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/I18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		
+		return messageSource();
 	}
 
 }
